465b45851c054e3d9b33e347758263aca1cabd7b
nassipkaliv <nassipkaliv@gmail.com>
2025-05-17 15:16:47 +0500

    section

--- js/app.js
+++ js/app.js
@@ -1,12 +1,35 @@
-  document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('mouseenter', () => {
-      link.classList.add('active');
-    });
-    link.addEventListener('mouseleave', () => {
-      if (!link.classList.contains('permanent-active')) {
-        link.classList.remove('active');
-      }
-    });
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('mouseenter', () => {
+    link.classList.add('active');
   });
+  link.addEventListener('mouseleave', () => {
+    if (!link.classList.contains('permanent-active')) {
+      link.classList.remove('active');
+    }
+  });
+});
+document.querySelector('.nav-link.active')?.classList.add('permanent-active');
 
-  document.querySelector('.nav-link.active')?.classList.add('permanent-active');
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
